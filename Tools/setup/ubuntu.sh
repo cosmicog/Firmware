684f543328a17841399679321bc4f0c60ba4582e
Hamish Willee <hamishwillee@gmail.com>
2018-05-24 23:54:26 -0700

    conditional functionality support

--- Tools/setup/ubuntu.sh
+++ Tools/setup/ubuntu.sh
@@ -21,6 +21,54 @@ then
 	echo "WARNING, instructions only tested on Ubuntu 16.04"
 fi
 
+
+# Parameter handling
+#echo "I was called with $# parameters"
+echo "Script supports the following flags to enable features: --gazebo --jmavsim --rtps --ros --nuttx"
+while [ "$#" -gt "0" ]
+do
+    case "$1" in
+        --gazebo) 
+            #echo "Gazebo"
+            _enable_gazebo=true
+            ;;
+        --jmavsim) 
+            #echo "jMAVSim"
+            _enable_jmavsim=true
+            ;;
+        --ros) 
+            #echo "ROS/Gazebo"
+            _enable_ros_gazebo=true
+            ;;
+        --rtps) 
+            #echo "RTPS"
+            _enable_rtps=true
+            ;;
+        --ros) 
+            #echo "nuttx"
+            _enable_nuttx=true
+            ;;
+        *) echo " WARNING: Bad option: $1"
+            ;;
+    esac
+    shift
+done
+
+if [[ ${_enable_gazebo} && ${_enable_ros_gazebo} ]]
+then 
+      _enable_gazebo=''
+      echo " IGNORED: --gazebo (included in --ros)"
+fi
+
+echo "Enabled Features"
+if [[ ${_enable_gazebo} ]];then echo _enable_gazebo: ${_enable_gazebo};fi
+if [[ ${_enable_jmavsim} ]];then echo _enable_jmavsim: ${_enable_jmavsim};fi
+if [[ ${_enable_ros_gazebo} ]];then echo _enable_ros_gazebo: ${_enable_ros_gazebo};fi
+if [[ ${_enable_rtps} ]];then echo _enable_rtps: ${_enable_rtps};fi
+if [[ ${_enable_nuttx} ]];then echo _enable_nuttx: ${_enable_nuttx};fi
+
+
+
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -yy --quiet
 sudo apt-get -yy --quiet --no-install-recommends install \
@@ -46,8 +94,22 @@ sudo apt-get -yy --quiet --no-install-recommends install \
 python -m pip install --user --upgrade pip setuptools wheel
 python -m pip install --user -r ${DIR}/requirements.txt
 
+
 # java (jmavsim or fastrtps)
-# TODO: only install when necessary
-sudo apt-get -y --quiet --no-install-recommends install \
+if [[ ${_enable_jmavsim} || ${_enable_ros_gazebo} ]]
+then 
+    sudo apt-get -y --quiet --no-install-recommends install \
 	default-jre-headless \
 	default-jdk-headless \
+fi
+
+
+
+
+# Disable all feature variables - in case file is called multiple times in same context:
+_enable_gazebo=''
+_enable_jmavsim=''=''
+_enable_ros_gazebo=''
+_enable_rtps=''
+_enable_nuttx=''
+
